6668c9ccc09ebcb1b596f9330d180d8e47c25e78
juyiz <juyi.zhu@student.unimelb.edu.au>
2026-01-03 12:41:07 +1300

    Initial commit

---
+++ src/main/java/net/racquo/raccoonsCo/RaccoonsCoClient.java
@@ -0,0 +1,10 @@
+package net.racquo.raccoonsCo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RaccoonsCoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/racquo/raccoonsCo/RaccoonsCoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.racquo.raccoonsCo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RaccoonsCoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
